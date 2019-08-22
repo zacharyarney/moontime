de37e0d480a8e4b603859921afeddde6c2a82a75
zacharyarney <zachary.arney@gmail.com>
2019-08-21 22:03:40 -0700

    initial commit -- CRA, socket.io, eslint

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { ChatBox } from './components/ChatBox/ChatBox';
+
+const App = () => {
+  return (
+    <div className="App">
+      App goes here
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
