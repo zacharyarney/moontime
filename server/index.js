be664cf0bd975e73df8255010d4942440571ef79
zacharyarney <zachary.arney@gmail.com>
2019-10-02 21:33:22 -0700

    Cleans up index.js continued...

--- server/index.js
+++ server/index.js
@@ -1,43 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const server = require('./server.js');
 
-const server = express();
+const port = 5000
 
-server.use(express.json()); // middleware for parsing JSON
-server.use(cors());
-
-const httpServer = require('http').Server(server);
-const io = require('socket.io')(httpServer);
-
-const messages = [
-  {
-    id: 1,
-    user: 'User 1',
-    timestamp: Date.now(),
-    body: 'Hello this is message',
-  },
-  {
-    id: 2,
-    user: 'User 2',
-    timestamp: Date.now(),
-    body: 'Hello this is also message',
-  },
-  {
-    id: 3,
-    user: 'User 1',
-    timestamp: Date.now(),
-    body: 'Indeed',
-  },
-];
-
-server.get('/', (req, res) => {
-  res.status(200).json({ messages });
-});
-
-io.on('connection', () => {
-  console.log('user connected wowee');
-});
-
-httpServer.listen(5000, () => {
-  console.log('\n===SERVER IS LISTENING ON 5000===\n');
+server.listen(port, () => {
+  console.log(`\n===SERVER IS LISTENING ON ${port}===\n`);
 });
