f3bdb11bdd85294f1b7052c8dc4fbce8892f9197
zacharyarney <zachary.arney@gmail.com>
2019-10-02 21:34:42 -0700

    Adds socket listener in MessageWindow

--- client/src/components/ChatBox/MessageWindow/MessageWindow.js
+++ client/src/components/ChatBox/MessageWindow/MessageWindow.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { endpoint } from '../../../util/socketStuff';
+import { endpoint, socket } from '../../../util/socketStuff';
 
 export const MessageWindow = () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     axios.get(endpoint).then((res) => {
-      console.log(res);
+      // console.log(res);
       setMessages(res.data.messages);
     });
   }, []);
+
+  useEffect(() => {
+    socket.on('messages', (data) => {
+      setMessages([...messages, data]);
+    });
+    console.log(messages);
+  }, [messages]);
 
   return (
     <div>
