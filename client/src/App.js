cba8ef7cbbe12369f0699f7cbafa1ee6322a7cb9
zacharyarney <zachary.arney@gmail.com>
2019-10-04 20:11:01 -0700

    Adds holdover login functionality on App.js to allow for usernames

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { ChatBox } from './components/ChatBox/ChatBox';
+import ChatBox from './components/ChatBox/ChatBox';
+import Login from './components/Login/Login';
 
 const App = () => {
+  const [userName, setUserName] = useState('');
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('userName')) {
+      setUserName(localStorage.getItem('userName'));
+    }
+  }, []);
+
+  const handleInput = (e) => setValue(e.target.value);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setUserName(value);
+    localStorage.setItem('userName', value);
+  };
+
   return (
     <div className="App">
-      <ChatBox />
+      {userName ? (
+        <ChatBox />
+      ) : (
+        <Login handleLogin={handleLogin} handleInput={handleInput} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
